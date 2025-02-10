9d505667b052662f11a3b5a41de10445593b1928
aamna-ansari <aamnansari29@gmail.com>
2025-02-10 22:39:05 +0500

    set up EJS view engine and configure views directory in index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Step 01: Express Import
 const express = require("express");
 
+// Step 04: Path import
+const path = require('path');
+
 // Step 02: Create instant
 const app = express();
+
+// Step 03: EJS
+
+app.set('view engine','ejs')
+app.set('views', path.resolve('./views'))
 
 // Port
 const PORT = 8000;
