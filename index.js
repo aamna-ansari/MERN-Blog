a1a588b4cb358c9b7884e102e68f0ffac54c4ef9
aamna-ansari <aamnansari29@gmail.com>
2025-02-21 20:23:20 +0500

    feat: connect to MongoDB and update signup form action

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 // Step 01: Express Import
 const express = require("express");
+// Import Mongoose;
+const mongoose = require("mongoose");
 
 // Import User From Routes
-const userRoute = require('./routes/user');
+const userRoute = require("./routes/user");
 
 // Step 03: Path import
-const path = require('path');
+const path = require("path");
+const { log } = require("console");
 
 // Step 02: Create instant
 const app = express();
 
-// Step 04: EJS
-app.set('view engine','ejs')
-app.set('views', path.resolve('./views'));
+// Conenct Mongoose
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blogsphere")
+  .then((e) => console.log("MongooseDB Connected"));
 
+// Step 04: EJS
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+// middlware for form handle 
+app.use(express.urlencoded({extended:false}));
 
 // For test Routes
-app.get('/', (req,res)=>{
-    res.render('home');
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 // UseRoute
-app.use('/user', userRoute);
-
-
+app.use("/user", userRoute);
 
 // Port
 const PORT = 8000;
