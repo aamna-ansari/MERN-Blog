5b7bb9678d88240a8cb0d7b7b8fb5c9485000465
aamna-ansari <aamnansari29@gmail.com>
2025-02-21 15:23:12 +0500

    feat: add signup route and improve signin route documentation

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,14 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get('/signin',(req,res)=>{
-    return res.render('signin')
+// Get request for SignIn
+router.get("/signin", (req, res) => {
+  return res.render("signin");
+});
+
+// Get request for SignUp
+router.get("/signup", (req, res) => {
+  return res.render("signup");
 });
 
 module.exports = router;
